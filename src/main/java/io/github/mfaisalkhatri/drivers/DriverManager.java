88a1b4bc840b0e147088e67c41a3a94fc64f76e9
WasiqB <wasbhamla2005@gmail.com>
2022-07-08 18:16:46 +0530

    Updated for device id.

--- src/main/java/io/github/mfaisalkhatri/drivers/DriverManager.java
+++ src/main/java/io/github/mfaisalkhatri/drivers/DriverManager.java
@@ -1,20 +1,17 @@
 package io.github.mfaisalkhatri.drivers;
 
 import static java.text.MessageFormat.format;
+import static java.util.Objects.requireNonNull;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
-
 import java.util.concurrent.TimeUnit;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-
 import lombok.Builder;
 import lombok.SneakyThrows;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -22,12 +19,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 @Builder
 public class DriverManager {
 
-    private static final ThreadLocal<AppiumDriver<MobileElement>> DRIVER = new ThreadLocal<> ();
-
-    private static final String LT_USERNAME     = System.getenv ("username");
-    private static final String LT_ACCESS_TOKEN = System.getenv ("token");
-    private static final String GRID_URL        = "@mobile-hub.lambdatest.com/wd/hub";
-    private              int    capsIndex;
+    private static final ThreadLocal<AppiumDriver<MobileElement>> DRIVER          = new ThreadLocal<> ();
+    private static final String                                   GRID_URL        = "@mobile-hub.lambdatest.com/wd/hub";
+    private static final String                                   LT_ACCESS_TOKEN = System.getenv ("token");
+    private static final String                                   LT_USERNAME     = System.getenv ("username");
+    private              String                                   deviceId;
 
     @SneakyThrows
     public DriverManager createRemoteDriver () {
@@ -54,47 +50,50 @@ public class DriverManager {
         }
     }
 
+    private DesiredCapabilities capabilities () {
+        final DesiredCapabilities capabilities = new DesiredCapabilities ();
+        try (
+            final var in = new FileInputStream (requireNonNull (getClass ().getClassLoader ()
+                .getResource ("parallel.config.json")).getPath ())) {
+
+            final var objectMapper = new ObjectMapper ();
+            final JsonNode jsonNode = objectMapper.readValue (in, JsonNode.class);
+
+            final var devices = jsonNode.get ("deviceAndBuildCaps")
+                .elements ();
+            devices.forEachRemaining (device -> {
+                if (device.get ("id")
+                    .asText ()
+                    .equals (this.deviceId)) {
+                    device.fields ()
+                        .forEachRemaining (capability -> {
+                            if (!capability.getKey ()
+                                .equals ("id")) {
+                                capabilities.setCapability (capability.getKey (), capability.getValue ()
+                                    .asText ());
+                            }
+                        });
+                }
+            });
+
+            final JsonNode capabilitiesNode = jsonNode.get ("commonCapabilities");
+            capabilitiesNode.fields ()
+                .forEachRemaining (capsEntry -> {
+                    final String commonCapsfieldName = capsEntry.getKey ();
+                    final String commonCapsfieldValue = capsEntry.getValue ()
+                        .asText ();
+                    capabilities.setCapability (commonCapsfieldName, commonCapsfieldValue);
+                });
+        } catch (final IOException e) {
+            e.printStackTrace ();
+        }
+        return capabilities;
+    }
+
     private void setupDriverTimeouts () {
         getDriver ().manage ()
             .timeouts ()
             .implicitlyWait (30, TimeUnit.SECONDS);
-    }
-
-    private DesiredCapabilities capabilities () {
-        DesiredCapabilities capabilities = new DesiredCapabilities ();
-        try (
-            FileInputStream in = new FileInputStream (getClass ().getClassLoader ()
-                .getResource ("parallel.config.json")
-                .getPath ())) {
-
-            ObjectMapper objectMapper = new ObjectMapper ();
-            JsonNode jsonNode = objectMapper.readValue (in, JsonNode.class);
-
-            JsonNode deviceInfoNode = jsonNode.get ("deviceAndBuildCaps")
-                .get (capsIndex);
-
-            deviceInfoNode.fields ()
-                .forEachRemaining (entry -> {
-                    String fieldName = entry.getKey ();
-                    String fieldValue = entry.getValue ()
-                        .asText ();
-                    capabilities.setCapability (fieldName, fieldValue);
-                });
-
-            JsonNode capabilitiesNode = jsonNode.get ("commoncapabilities");
-            capabilitiesNode.fields ()
-                .forEachRemaining (capsEntry -> {
-                    String commonCapsfieldName = capsEntry.getKey ();
-                    String commonCapsfieldValue = capsEntry.getValue ()
-                        .asText ();
-                    capabilities.setCapability (commonCapsfieldName, commonCapsfieldValue);
-                });
-        } catch (FileNotFoundException e) {
-            e.printStackTrace ();
-        } catch (IOException e) {
-            e.printStackTrace ();
-        }
-        return capabilities;
     }
 
 }
